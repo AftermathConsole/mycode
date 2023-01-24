61f8f72f92c39b1d52e10a06bd5118a1f779d22f
AftermathConsole <isoosi@verizon.net>
2023-01-24 16:02:12 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Lab 18 list of animals, making selections."""
+
+def main():
+
+    # create a new list of five animals with 3-letter names
+    animals = [ "ant", "emu", "ape", "eel", "dog", "cat"]
+
+    # dislay the list onscreen
+    print(animals)
+
+main ()
