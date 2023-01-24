731b295a27c3ebc436b89ba4181b48ac762c1290
AftermathConsole <isoosi@verizon.net>
2023-01-24 15:14:15 +0000

    manipulating lists

--- listmethods/listmeth02.py
+++ listmethods/listmeth02.py
@@ -6,6 +6,7 @@ proto.append("dns") # this line will add "dns" to the end of our list
 protoa.append("dns") # this line will add "dns" to the end of our list
 print(proto)
 proto2 = [ 22, 80, 443, 53 ] # a list of common ports
+proto.insert(1, "ftp")
 proto.extend(proto2) # pass proto2 as an argument to the extend method
 print(proto)
 protoa.append(proto2) # pass proto2 as an argument to the append method
