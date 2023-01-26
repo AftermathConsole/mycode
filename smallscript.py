dbf417342374d88d7801d28391a35ad8df73c02a
AftermathConsole <isoosi@verizon.net>
2023-01-26 20:28:45 +0000

    Pylint exercise

---
+++ smallscript.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Author: Me
+
+This program prints some things to the screen
+"""
+
+def main():
+    """Our main program"""
+MYNOTE = "print this string" # define mynote
+print(MYNOTE)    # display our string
+
+main()
