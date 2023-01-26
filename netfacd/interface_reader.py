86d9e39a7820007f10f833e3fa6aeaf080e93806
AftermathConsole <isoosi@verizon.net>
2023-01-26 14:42:03 +0000

    Python and physical interfaces

--- netfacd/interface_reader.py
+++ netfacd/interface_reader.py
@@ -9,10 +9,10 @@ for i in netifaces.interfaces():
     print('\n**************Details of Interface - ' + i + ' *********************')
     try:
         print('MAC: ', end='') # This print statement will always print MAC without an end of line
-        print(netifaces.ifaddresses(i)[netifaces.AF_LINK][0]['addr']) # prints the MAC address
+        print(netifaces.ifaddresses(i)[netifaces.AF_LINK][0]['addr']) # prints the MAC address without an end of line
         # uses the module netifaces, tool ifaddresses of (interfacename) then uses the tool AF_LINK, then chooses the device at
         # index [0], and types addr
         print('IP: ', end='')
-        print(netifaces.ifaddresses(i)[netifaces.AF_INET][0]['addr']) # prints the IP address
+        print(netifaces.ifaddresses(i)[netifaces.AF_INET][0]['addr']) # prints the IP address without an end of line
     except: # this is a new line
         print('Could not collect adapter information') # print an error message.
